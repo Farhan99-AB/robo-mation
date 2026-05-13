f5efb488be8918c62d8650c800bef48b404da359
Prime-cmd <83109533+Prime-cmd@users.noreply.github.com>
2026-05-13 21:16:32 +0200

    ros dir cleanup

--- ROS2/src/dual_arm_simulation/build/dual_arm_simulation/prefix_override/sitecustomize.py
+++
@@ -1,4 +0,0 @@
-import sys
-if sys.prefix == '/usr':
-    sys.real_prefix = sys.prefix
-    sys.prefix = sys.exec_prefix = '/root/ROS/Group_3_2026/ros2_lab_ws/src/dual_arm_simulation/install/dual_arm_simulation'
